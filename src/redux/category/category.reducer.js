ace3b09e7d89819c1f641cfb4e42b374e19c7815
hyPham-uit <hypham3012@gmail.com>
2021-05-10 21:01:01 +0700

    add login screen to others screen

--- src/redux/category/category.reducer.js
+++ src/redux/category/category.reducer.js
@@ -1,5 +1,5 @@
 import CATEGORY_DATA from "./category.data";
-
+//import getCollection from "../../api/collection";
 const INITIAL_STATE = CATEGORY_DATA;
 
 const categoryReducer = (state = INITIAL_STATE, action) => {
