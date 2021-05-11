4980b7c4b876b27e723d6ec9c33a461f2f94f26c
quanguit <nhatquang25012000@gmail.com>
2021-05-11 16:55:47 +0700

    hac

--- src/screens/Others.Screen.jsx
+++ src/screens/Others.Screen.jsx
@@ -2,103 +2,111 @@ import React from "react";
 import { Text } from "react-native";
 import { View, ActivityIndicator } from "react-native";
 //import BottomBar from "./src/component/bar/bottom-bar";
-import RootStackScreen from '../screens/RootStackScreen';
-import { NavigationContainer} from '@react-navigation/native';
-import { AuthContext } from '../contexts/context';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import RootStackScreen from "../screens/RootStackScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthContext } from "../contexts/context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const OthersScreen = () =>{
-    const initialLoginState = {
-        isLoading: false,
-        userName: null,
-        userToken: null,
-    };
+const OthersScreen = () => {
+  const initialLoginState = {
+    isLoading: false,
+    userName: null,
+    userToken: null,
+  };
 
-    const loginReducer = (prevState, action) => {
-    switch( action.type ) {
-        case 'RETRIEVE_TOKEN': 
+  const loginReducer = (prevState, action) => {
+    switch (action.type) {
+      case "RETRIEVE_TOKEN":
         return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,
         };
-        case 'LOGIN': 
+      case "LOGIN":
         return {
-            ...prevState,
-            userName: action.id,
-            userToken: action.token,
-            isLoading: false,
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
         };
-        case 'LOGOUT': 
+      case "LOGOUT":
         return {
-            ...prevState,
-            userName: null,
-            userToken: null,
-            isLoading: false,
+          ...prevState,
+          userName: null,
+          userToken: null,
+          isLoading: false,
         };
-        case 'REGISTER': 
+      case "REGISTER":
         return {
-            ...prevState,
-            userName: action.id,
-            userToken: action.token,
-            isLoading: false,
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
         };
     }
-    };
+  };
 
-    const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+  const [loginState, dispatch] = React.useReducer(
+    loginReducer,
+    initialLoginState
+  );
 
-    const authContext = React.useMemo(() => ({
-      signIn: async(foundUser) => {
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async (foundUser) => {
         // setUserToken('fgkj');
         // setIsLoading(false);
         const userToken = String(foundUser[0].userToken);
         const userName = foundUser[0].username;
-        
+
         try {
-          await AsyncStorage.setItem('userToken', userToken);
-        } catch(e) {
+          await AsyncStorage.setItem("userToken", userToken);
+        } catch (e) {
           console.log(e);
         }
         // console.log('user token: ', userToken);
-        dispatch({ type: 'LOGIN', id: userName, token: userToken });
+        dispatch({ type: "LOGIN", id: userName, token: userToken });
       },
-      signOut: async() => {
+      signOut: async () => {
         // setUserToken(null);
         // setIsLoading(false);
         try {
-          await AsyncStorage.removeItem('userToken');
-        } catch(e) {
+          await AsyncStorage.removeItem("userToken");
+        } catch (e) {
           console.log(e);
         }
-        dispatch({ type: 'LOGOUT' });
+        dispatch({ type: "LOGOUT" });
       },
       signUp: () => {
         // setUserToken('fgkj');
         // setIsLoading(false);
       },
       toggleTheme: () => {
-        setIsDarkTheme( isDarkTheme => !isDarkTheme );
-      }
-    }), []);
+        setIsDarkTheme((isDarkTheme) => !isDarkTheme);
+      },
+    }),
+    []
+  );
 
-    if( loginState.isLoading ) {
-        return(
-          <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <ActivityIndicator size="large"/>
-          </View>
-        );
-    }
+  if (loginState.isLoading) {
     return (
-        <AuthContext.Provider value={authContext}>
-            <NavigationContainer independent={true}>
-                { 
-                    loginState.userToken !== null ? ( <Text>Others</Text> ) :<RootStackScreen/>
-                }
-            </NavigationContainer>
-        </AuthContext.Provider>
-        //<BottomBar />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
     );
-} //<Text>Others</Text>;
+  }
+  return (
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer independent={true}>
+        {loginState.userToken !== null ? (
+          <Text>Others</Text>
+        ) : (
+          <RootStackScreen />
+        )}
+      </NavigationContainer>
+    </AuthContext.Provider>
+    //<BottomBar />
+  );
+}; //<Text>Others</Text>;
 
 export default OthersScreen;
