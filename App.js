203060f905624381a75bbc8eacadaa673f8c5b75
quanguit <nhatquang25012000@gmail.com>
2021-05-20 15:05:04 +0700

    dau

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import BottomBar from './src/component/bar/bottom-bar';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './src/redux/root-reducer';
-import logger from 'redux-logger';
+import React from "react";
+import BottomBar from "./src/component/bar/bottom-bar";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./src/redux/root-reducer";
+import logger from "redux-logger";
 
 const middlewares = [logger];
 const enhancers = applyMiddleware(...middlewares);
 const store = createStore(rootReducer, enhancers);
 
 function App() {
-	return <BottomBar />;
+  return <BottomBar />;
 }
 
 export default () => {
-	return (
-		<Provider store={store}>
-			<App />
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 };
